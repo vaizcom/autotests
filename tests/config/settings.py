a40439f68d49c2985da3e4475e0581063e138e97
marina <mastretsova@ibitcy.com>
2025-06-13 12:29:40 +0300

    ``` Refactor test workflow and improve environment handling

--- tests/config/settings.py
+++ tests/config/settings.py
@@ -15,7 +15,7 @@ USERS = {
     'owner': {'email': os.getenv('OWNER_EMAIL'), 'password': os.getenv('PASSWORD')},
 }
 
-TEST_STAND_NAME = os.getenv('TEST_STAND_NAME')
+TEST_STAND_NAME = os.getenv('TEST_STAND_NAME', "kuber_dev")
 
 API_URL = {
     'dev': 'https://api.vaiz.dev/v4',
@@ -23,3 +23,7 @@ API_URL = {
     'kuber_dev': 'https://vaiz-api-ms.vaiz.dev/v4',
     'kuber_uat': 'https://vaiz-api-uat.vaiz.dev/v4',
 }[TEST_STAND_NAME]
+
+if os.getenv("GITHUB_ENV"):
+    with open(os.getenv("GITHUB_ENV"), "a") as f:
+        f.write(f"API_URL={API_URL}\n")
