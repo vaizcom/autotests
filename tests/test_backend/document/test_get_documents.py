5f1a5691afc3b8a9bf46cc9b21504c7549f7f957
marina <mastretsova@ibitcy.com>
2025-06-20 10:33:13 +0300

    Update test to expect 'InvalidForm' instead of 'InvalidKind'

--- tests/test_backend/document/test_get_documents.py
+++ tests/test_backend/document/test_get_documents.py
@@ -49,6 +49,6 @@ def test_get_documents_with_wrong_kind(owner_client, temp_space, temp_project):
             **get_documents_endpoint(kind='WrongKind', kind_id=temp_project, space_id=temp_space)
         )
 
-    with allure.step('Ожидаем 400 ошибку валидации, codes: InvalidKind'):
+    with allure.step('Ожидаем 400 ошибку валидации, codes: InvalidForm'):
         assert response.status_code == 400
-        assert response.json()['error']['code'] == 'InvalidKind'
+        assert response.json()['error']['code'] == 'InvalidForm'
