a4c5d63607880cd4b4eb48ca5f79d9e6e582ce7b
marina <mastretsova@ibitcy.com>
2025-06-20 10:56:59 +0300

    Update test to expect 'InvalidForm' instead of 'InvalidKind'

--- tests/test_backend/document/test_create_document.py
+++ tests/test_backend/document/test_create_document.py
@@ -288,15 +288,3 @@ def test_create_document_in_foreign_space(owner_client, foreign_space, temp_proj
     with allure.step('Ожидаем 400, проверяем текст ошибки SpaceIdNotSpecified'):
         assert response.status_code == 400
         assert response.json()['error']['code'] == 'SpaceIdNotSpecified'
-
-
-@allure.title('Ошибка при запросе документов с некорректным kind')
-def test_get_documents_with_wrong_kind(owner_client, temp_space, temp_project):
-    with allure.step('Отправка запроса с несуществующим kind'):
-        response = owner_client.post(
-            **get_documents_endpoint(kind='WrongKind', kind_id=temp_project, space_id=temp_space)
-        )
-
-    with allure.step('Ожидаем 400 ошибку валидации, codes: InvalidForm'):
-        assert response.status_code == 400
-        assert response.json()['error']['code'] == 'InvalidForm'
