c7bf1c30730ceec4137eee192f0606af55e00dac
marina <mastretsova@ibitcy.com>
2025-08-21 15:00:29 +0300

    renamed space_client fixture to space_client_memb; updated related test for accuracy; added new test for owner_client document access

--- tests/test_backend/board/access_board/test_create_board.py
+++ tests/test_backend/board/access_board/test_create_board.py
@@ -5,7 +5,6 @@ import allure
 from test_backend.data.endpoints.Board.board_endpoints import (
     create_board_endpoint,
     delete_board_endpoint,
-    get_board_endpoint,
 )
 
 pytestmark = [pytest.mark.backend]
@@ -53,53 +52,3 @@ def test_create_board_access_by_roles(request, client_fixture, expected_status, 
                 delete_payload = delete_board_endpoint(board_id=board_id, board_name=board_name, space_id=main_space)
                 delete_response = client.post(**delete_payload)
                 assert delete_response.status_code == 200, 'Ошибка при удалении доски'
-
-
-@pytest.mark.parametrize(
-    'client_fixture, expected_status, expected_error_code',
-    [
-        ('foreign_client', 403, 'AccessDenied'),  # Пользователь не имеет доступа к space
-        ('space_client', 403, 'AccessDenied'),  # Пользователь имеет доступ к space, но не к проекту и борде
-        ('project_client', 403, 'AccessDenied'),  # Пользователь имеет доступ к space и проекту, но не к борде
-    ],
-    ids=['foreign_client', 'space_client', 'project_client'],
-)
-def test_negative_access_to_board(
-    request, client_fixture, expected_status, expected_error_code, main_board, main_space
-):
-    allure.dynamic.title(
-        f'Негативный тест на доступ к борде: клиент={client_fixture}, ожидаемый статус={expected_status}'
-    )
-
-    with allure.step(f'Получение клиента: {client_fixture}'):
-        client = request.getfixturevalue(client_fixture)
-
-    with allure.step(f"Отправка запроса на получение данных борды с ID '{main_board}'"):
-        payload = get_board_endpoint(board_id=main_board, space_id=main_space)
-        response = client.post(**payload)
-
-    with allure.step(f'Проверка статус-кода: ожидаемый {expected_status}'):
-        assert response.status_code == expected_status, response.text
-
-    with allure.step('Проверка структуры ответа'):
-        response_json = response.json()
-        assert 'payload' in response_json, "Отсутствует ключ 'payload' в ответе"
-        assert 'error' in response_json, "Отсутствует ключ 'error' в ответе"
-        assert 'type' in response_json, "Отсутствует ключ 'type' в ответе"
-
-    with allure.step("Проверка значения 'payload' (должно быть null)"):
-        assert response_json['payload'] is None, "Ожидался 'null' в поле 'payload'"
-
-    with allure.step("Проверка ошибки в поле 'error'"):
-        error = response_json['error']
-        assert 'code' in error, "Отсутствует ключ 'code' в 'error'"
-        assert 'originalType' in error, "Отсутствует ключ 'originalType' в 'error'"
-        assert (
-            error['code'] == expected_error_code
-        ), f"Ожидался код ошибки '{expected_error_code}', но получен '{error['code']}'"
-        assert (
-            error['originalType'] == 'GetBoard'
-        ), f"Ожидался originalType 'GetBoard', но получен '{error['originalType']}'"
-
-    with allure.step("Проверка значения 'type'"):
-        assert response_json['type'] == 'GetBoard', f"Ожидался 'type'='GetBoard', но получен '{response_json['type']}'"
--- tests/test_backend/document/test_archive_all_docs.py
+++ tests/test_backend/document/test_archive_all_docs.py
@@ -8,6 +8,14 @@ from test_backend.data.endpoints.Document.document_endpoints import (
 pytestmark = [pytest.mark.backend]
 
 
+@pytest.fixture(scope='session')
+def excluded_documents(main_space_doc, main_project_doc):
+    """
+    Возвращает множество исключаемых документов.
+    """
+    return {main_space_doc, main_project_doc}
+
+
 @pytest.mark.parametrize(
     'kind, container_fixture',
     [
@@ -16,7 +24,7 @@ pytestmark = [pytest.mark.backend]
     ],
     ids=['space_docs', 'project_docs'],
 )
-def test_archive_all_documents(request, owner_client, kind, container_fixture, main_space):
+def test_archive_all_documents(request, owner_client, kind, container_fixture, main_space, excluded_documents):
     """
     Проверяем удаление всех документов в пространстве и проекте.
     """
@@ -26,12 +34,14 @@ def test_archive_all_documents(request, owner_client, kind, container_fixture, m
     with allure.step(f'Получение списка документов в {kind}'):
         docs_resp = owner_client.post(**get_documents_endpoint(space_id=main_space, kind=kind, kind_id=container_id))
         assert docs_resp.status_code == 200, 'Не удалось получить список документов'
-
         # Получаем список ID документов
         doc_ids = [doc['_id'] for doc in docs_resp.json()['payload']['documents']]
 
-    with allure.step(f'Удаление всех документов в {kind}'):
+    with allure.step(f'Удаление всех документов в {kind}, кроме исключений'):
         for doc_id in doc_ids:
+            if doc_id in excluded_documents:
+                allure.step(f'Пропуск документа {doc_id}, т.к. он в списке исключений.')
+                continue
             archive_resp = owner_client.post(**archive_document_endpoint(document_id=doc_id, space_id=main_space))
             assert archive_resp.status_code == 200, (
                 f'Не удалось архивировать документ {doc_id}: ' f'статус {archive_resp.status_code}'
