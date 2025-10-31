c855676dffafa0b3b8f9262ea86092a88becb41c
marina <mastretsova@ibitcy.com>
2025-10-31 13:34:12 +0200

    Fix

--- tests/test_backend/data/endpoints/User/profile_endpoint.py
+++ tests/test_backend/data/endpoints/User/profile_endpoint.py
@@ -2,7 +2,6 @@ def get_profile_endpoint(space_id: str):
     return {
         'path': '/getProfile',
         'headers': {
-            'Content-Type': 'application/json',
             'Current-Space-Id': space_id,
         },
     }
